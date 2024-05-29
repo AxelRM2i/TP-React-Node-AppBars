219d16bce91655c6a53d5f4a1cd84c0b7a7b7bf9
Axel <167887112+AxelRM2i@users.noreply.github.com>
2024-05-29 16:41:46 +0200

    Add files via upload
    Signed-off-by: Axel <167887112+AxelRM2i@users.noreply.github.com>

---
+++ front2/src/App.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Menu from './components/menu';
+import Manager from './components/manager';
+import Thumb from './components/thumb';
+import HomePage from './pages/homepage';
+import BarList from './pages/barList';
+import "./App.css";
+// import ListeBieresPage from './pages/ListeBieres';
+// import ListeCommandesPage from './pages/ListeCommandes';
+
+const App = () => {
+    return (
+        <Router>
+            <>
+                <Menu />
+                <Manager />
+                <Thumb />
+                <Routes>
+                    <Route path="/" element={<HomePage />} />
+                    <Route path="/bars" element={<BarList />} />
+                    {/* <Route path="/bieres" element={<ListeBieresPage />} />
+                    <Route path="/commandes" element={<ListeCommandesPage />} /> */}
+                </Routes>
+            </>
+        </Router>
+    );
+};
+
+export default App;
