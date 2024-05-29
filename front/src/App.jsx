65de9e58c796297c6aaaa7931e005d29479fb40f
Axel Ressot <axel.ressot@labom2iformation.fr>
2024-05-29 17:00:26 +0200

    update front

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Menu from './components/menu';
+import Manager from './components/manager';
+import Thumb from './components/thumb';
 import HomePage from './pages/homepage';
 import BarList from './pages/barList';
 import "./App.css";
@@ -8,20 +10,23 @@ import "./App.css";
 // import ListeCommandesPage from './pages/ListeCommandes';
 
 const App = () => {
-    return (
-        <Router>
-            <>
-                <Menu />
-                <Manager />
-                <Routes>
-                    <Route path="/" element={<HomePage />} />
-                    <Route path="/bars" element={<BarList />} />
-                    {/* <Route path="/bieres" element={<ListeBieresPage />} />
-                    <Route path="/commandes" element={<ListeCommandesPage />} /> */}
-                </Routes>
-            </>
-        </Router>
-    );
+  return (
+    <Router>
+      <>
+        <Menu />
+        <main>
+          <Thumb />
+          <Manager />
+          <Routes>
+              <Route path="/" element={<HomePage />} />
+              <Route path="/bars" element={<BarList />} />
+              {/* <Route path="/bieres" element={<ListeBieresPage />} />
+              <Route path="/commandes" element={<ListeCommandesPage />} /> */}
+          </Routes>
+        </main>
+      </>
+    </Router>
+  );
 };
 
 export default App;
---
+++ front/src/components/thumb.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Thumb = () => {
+    return (
+        <>
+            <div className="thumb">
+                <div className="thumbImg"></div>
+                <h1>Gérez vos bars et commandes, simplement.</h1>
+            </div>
+        </>
+    )
+}
+
+export default Thumb;
